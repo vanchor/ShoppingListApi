39ecc132fb3c6edfc464c3556f2f37c4840a5fc2
vanchor <vovanyakorev2@gmail.com>
2022-08-28 15:09:33 +0200

    Added error handler

---
+++ ShoppingListApi/Controllers/ErrorsController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ShoppingListApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ErrorsController : ControllerBase
+    {   
+        [Route("/error-development")]
+        public IActionResult HandleErrorDevelopment([FromServices] IHostEnvironment hostEnvironment)
+        {
+            if (!hostEnvironment.IsDevelopment())
+            {
+                return NotFound();
+            }
+
+            var exeptionHandlerFeature = HttpContext.Features.Get<IExceptionHandlerFeature>()!;
+
+            return Problem(detail: exeptionHandlerFeature.Error.StackTrace,
+                title: exeptionHandlerFeature.Error.Message);
+        }
+
+        [Route("/error")]
+        public IActionResult HandleError()
+        {
+            return Problem();
+        }
+    }    
+}
--- ShoppingListApi/Program.cs
+++ ShoppingListApi/Program.cs
@@ -19,6 +19,12 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseExceptionHandler("/error-development");
+
+}
+else
+{
+    app.UseExceptionHandler("/error");
 }
 
 app.UseHttpsRedirection();
