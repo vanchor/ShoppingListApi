7bfda208e0ac28bbc6724be73139a4fbd97e4810
vanchor <vovanyakorev2@gmail.com>
2022-08-28 12:22:21 +0200

    Added Grocery Model

---
+++ ShoppingListApi/Models/ShoppingListContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShoppingListApi.Models
+{
+    public class ShoppingListContext : DbContext
+    {
+        public DbSet<Grocery> Grocery { get; set; }
+
+        public ShoppingListContext(DbContextOptions<ShoppingListContext> options) : base(options)
+        {
+
+        }
+    }
+}
