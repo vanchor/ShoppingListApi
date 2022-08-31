e14e01f0f6f21973b9c1cd7ee5ec420f4f602d6f
vanchor <vovanyakorev2@gmail.com>
2022-08-31 20:01:09 +0200

    Init new minimalApi project

--- ShoppingListApi/Program.cs
+++ ShoppingListApi/Program.cs
@@ -4,8 +4,8 @@ using ShoppingListApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
