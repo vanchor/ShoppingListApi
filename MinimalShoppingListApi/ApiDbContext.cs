81ff8704cc60d9d8e499e2c76ccec340e709cf40
vanchor <vovanyakorev2@gmail.com>
2022-08-31 20:13:49 +0200

    Added DbContext and models

---
+++ MinimalShoppingListApi/ApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimalShoppingListApi
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Grocery> Groceries { get; set; }
+    }
+}
--- MinimalShoppingListApi/Program.cs
+++ MinimalShoppingListApi/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using MinimalShoppingListApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApiDbContext>(opt => opt.UseInMemoryDatabase("ShoppingListApi"));
 
 var app = builder.Build();
 
